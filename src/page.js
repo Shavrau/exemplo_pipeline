c71c1077c703e8928603716f8fc83e35ea143c70
shavrau <rhianmendes301@gmail.com>
2024-08-22 09:44:18 -0300

    alteração visual

--- src/page.js
+++ src/page.js
@@ -1,5 +1,8 @@
-  document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     const deployTimeElement = document.getElementById('deploy-time');
+    const calculateButton = document.getElementById('calculate');
+    const resultElement = document.getElementById('result');
+    const audioElement = document.getElementById('background-audio');
     
     function formatDateTime(date) {
         return date.toLocaleString('pt-BR', {
@@ -12,23 +15,29 @@
         });
     }
 
-    const now = new Date();
-    const formattedDateTime = formatDateTime(now);
+    function getFormattedDeployTime() {
+        const deployTime = localStorage.getItem('deployTime');
+        if (deployTime) {
+            return deployTime;
+        } else {
+            const now = new Date();
+            const formattedDateTime = formatDateTime(now);
+            localStorage.setItem('deployTime', formattedDateTime);
+            return formattedDateTime;
+        }
+    }
+
+    const formattedDateTime = getFormattedDeployTime();
     deployTimeElement.textContent = `Este deploy foi feito em ${formattedDateTime}.`;
 
     function soma() {
-        return "Hello World"; // Mantendo a função como estava antes
+        return "Hello World";
     }
-
-    const calculateButton = document.getElementById('calculate');
-    const resultElement = document.getElementById('result');
-    const audioElement = document.getElementById('background-audio');
 
     calculateButton.addEventListener('click', () => {
         const result = soma(); 
         resultElement.textContent = `Resultado: ${result}`;
 
-        // Reproduzir áudio ao clicar no botão
         if (audioElement) {
             audioElement.play().catch(error => {
                 console.log('A reprodução de áudio falhou:', error);
@@ -36,7 +45,6 @@
         }
     });
 
-    // Efeitos malucos
     function shake() {
         document.body.classList.add('shake');
         setTimeout(() => document.body.classList.remove('shake'), 500);
